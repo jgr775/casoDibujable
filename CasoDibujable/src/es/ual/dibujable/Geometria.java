d5b8fb10a1e6935bb746ff6f7a5f21bf3defa0b4
Jose Omar <joseomargalafatrdguez@gmail.com>
2016-04-11 17:57:20 +0200

    Creación del proyecto

---
+++ CasoDibujable/src/es/ual/dibujable/Geometria.java
@@ -0,0 +1,8 @@
+package es.ual.dibujable;
+
+public abstract class Geometria {
+	// clase abstracta que no puede tener objetos
+	public abstract double perimetro();
+
+	public abstract double area();
+}
